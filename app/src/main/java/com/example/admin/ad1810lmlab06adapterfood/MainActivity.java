0a95b1a4172ad447cde6ac54f103f152aa55f9dd
Que Pham <adnetplus.dev@gmail.com>
2018-05-02 20:52:19 +0700

    Action Bar Menu

--- app/src/main/java/com/example/admin/ad1810lmlab06adapterfood/MainActivity.java
+++ app/src/main/java/com/example/admin/ad1810lmlab06adapterfood/MainActivity.java
@@ -2,8 +2,16 @@ package com.example.admin.ad1810lmlab06adapterfood;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.PopupMenu;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -71,7 +79,89 @@ public class MainActivity extends AppCompatActivity {
         // Cập nhật dữ liệu
         // 1. cập nhật trên List
         // 2. gọi adapter và hàm notify
-        adapterFood.notifyDataSetChanged();
+        // adapterFood.notifyDataSetChanged();
 
+        // Gán Context Menu cho ListView
+        registerForContextMenu(lstFood);
+
+        // Cài đặt sự kiện khi click vào item trên ListView => PopupMenu
+        lstFood.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                PopupMenu popupMenu = new PopupMenu(MainActivity.this, view);
+                popupMenu.inflate(R.menu.demo_menu);
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        int idMenu = item.getItemId();
+                        switch (idMenu) {
+                            case R.id.menu_them:
+                                Toast.makeText(MainActivity.this, "Thêm", Toast.LENGTH_SHORT).show();
+                                break;
+                            case R.id.menu_sua:
+                                Toast.makeText(MainActivity.this, "Sửa", Toast.LENGTH_SHORT).show();
+                                break;
+                            case R.id.menu_xoa:
+                                Toast.makeText(MainActivity.this, "Xóa", Toast.LENGTH_SHORT).show();
+                                break;
+                        }
+
+                        return true;
+                    }
+                });
+                popupMenu.show();
+            }
+        });
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        menu.setHeaderTitle("Lựa chọn");
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.demo_menu, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        int idMenu = item.getItemId();
+        switch (idMenu) {
+            case R.id.menu_them:
+                Toast.makeText(this, "Thêm", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menu_sua:
+                Toast.makeText(this, "Sửa", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menu_xoa:
+                Toast.makeText(this, "Xóa", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        return super.onContextItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.demo_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int idMenu = item.getItemId();
+        switch (idMenu) {
+            case R.id.menu_them:
+                Toast.makeText(this, "Thêm", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menu_sua:
+                Toast.makeText(this, "Sửa", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menu_xoa:
+                Toast.makeText(this, "Xóa", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
+
